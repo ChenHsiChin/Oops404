4518847200de9e0004a38bece99eb6fad2d9b931
CheneyJin <cheneyjin@outlook.com>
2018-05-08 18:17:38 +0800

    china adblock
    gene hosts about block china ads.

---
+++ adblock.sh
@@ -0,0 +1,5 @@
+curl -s -L https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt https://easylist-downloads.adblockplus.org/malwaredomains_full.txt https://easylist-downloads.adblockplus.org/fanboy-social.txt > adblock.unsorted
+sort -u adblock.unsorted | grep ^\|\|.*\^$ | grep -v \/ > adblock.sorted
+sed 's/[\|^]//g' < adblock.sorted > adblock.hosts
+rm adblock.unsorted adblock.sorted
+
